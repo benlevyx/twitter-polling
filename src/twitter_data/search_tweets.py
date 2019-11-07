850c24d0fe76d89b6917595302402a934d5863d5
levyxx <levy.b.j.s@gmail.com>
2019-11-07 17:07:27 -0500

    Fixed environment stuff

--- src/twitter_data/search_tweets.py
+++ src/twitter_data/search_tweets.py
@@ -1,14 +1,39 @@
 """
 search_tweets.py
 
-Using tweepy to acquire tweets from Twitter's search API
+Using search-twitter-python to acquire tweets from Twitter's Full Archive search API
+[sandbox mode]
 """
-import tweepy
+import os
 
-import config
-import utils
+import searchtweets as st
+
+from twitpol import config, utils
 
 
 # Loading API keys
 api_keys = utils.get_api_keys
 
+# Creating sandbox search args
+# search_args = {
+#     'endpoint': 'https://api.twitter.com/1.1/tweets/search/fullarchivev/dev.json',
+
+# }
+search_args = st.load_credentials(filename='not_a_file.yml', yaml_key='not_a_key')
+print(search_args)
+# Setting environment variables
+os.environ['SEARCHTWEETS_CONSUMER_KEY'] = api_keys['API_KEY']
+os.environ['SEARCHTWEETS_CONSUMER_SECRET'] = api_keys['API_KEY_SECRET']
+os.environ['SEARCHTWEETS_ENDPOINT'] = ''
+
+# Test query
+query = 'Bernie Sanders'
+
+# Generating query rule (5 to test)
+MAX_RESULTS = 5
+rule = st.gen_rule_payload(query, results_per_call=MAX_RESULTS)
+
+# Collecting tweets
+collected_tweets = st.collect_results(rule,
+                                      max_results=MAX_RESULTS,
+                                      result_stream_args=search_args)
