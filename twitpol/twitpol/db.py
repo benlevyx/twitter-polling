c98431052d8cbc01ed4da50761aa66736265fbc9
levyxx <levy.b.j.s@gmail.com>
2019-11-09 14:17:57 -0500

    Fixed db.py

--- twitpol/twitpol/db.py
+++ twitpol/twitpol/db.py
@@ -6,11 +6,12 @@ from twitpol import config, utils
 def get_db_engine(include_database=True, create_database=True):
     # TODO: Implement this (maybe in another module)
     db_params = config.DB_SETTINGS.copy()
-    db_str = f'mysql+pymysql://{db_params.user}:{db_params.password}@{db_params.host}/{db_params.port}/'
+    db_name = db_params.pop('database')
+    db_str = f'mysql+pymysql://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}/{db_params["port"]}/'
     if create_database:
         engine = create_engine(db_str)
-        engine.cursor().execute(f'CREATE DATABASE IF NOT EXISTS {db_params.database};')
+        engine.cursor().execute(f'CREATE DATABASE IF NOT EXISTS {db_name};')
     if include_database:
-        db_str += db_params.database
+        db_str += db_name
     engine = create_engine(db_str)
     return engine
